66ce925cb971dc84dcdfa5565a6f24e7051ad951
Daniel Hocking <z5184128@unsw.edu.au>
2018-09-15 17:31:34 +1000

    give on Sat 15 Sep 17:31:34 AEST 2018

--- legit.pl
+++ legit.pl
@@ -1,1 +1,100 @@
 #!/usr/bin/perl -w
+
+# Setup a script name without ./
+$script_name = $0;
+$script_name =~ s/\.\///;
+# If no arguments then show usage and exit
+if($#ARGV == -1) {
+  ::show_usage();
+}
+# If first argument doesnt match valid command then show usage and exit
+my $cmd = shift @ARGV;
+if(::valid_commands($cmd) == 0) {
+  ::show_usage();
+}
+
+if($cmd eq "init") {
+  ::init_command();
+}
+if($cmd eq "add") {
+  ::add_command();
+}
+
+sub show_usage {
+  print STDERR "Usage: legit.pl <command> [<args>]
+
+These are the legit commands:
+   init       Create an empty legit repository
+   add        Add file contents to the index
+   commit     Record changes to the repository
+   log        Show commit log
+   show       Show file at particular state
+   rm         Remove files from the current directory and from the index
+   status     Show the status of files in the current directory, index, and repository
+   branch     list, create or delete a branch
+   checkout   Switch branches or restore current directory files
+   merge      Join two development histories together
+";
+  exit 1;
+}
+
+sub valid_commands {
+  my ($cmd) = @_;
+  my %commands = (
+    "init"  => 1,
+    "add" => 1,
+  );
+  return defined $commands{$cmd} ? $commands{$cmd} : 0;
+}
+
+sub init_command {
+  my $init_dir = ".legit";
+  # Error case when more than one argument provided
+  if($#ARGV != -1) {
+    print STDERR "usage: $script_name init\n";
+    exit 1;
+  }
+  # Error case when dir already exists
+  if( -e $init_dir) {
+    print STDERR "$script_name: error: $init_dir already exists\n";
+    exit 1;
+  }
+  # Make .legit dir and an empty index file or error
+  mkdir $init_dir or (print STDERR "$script_name: error: could not create legit depository\n" and exit 1);
+  open my $f, ">", "$init_dir/index" or (print STDERR "$script_name: error: could not create legit depository\n" and exit 1);
+  print $f "";
+  close $f;
+  print "Initialized empty legit repository in $init_dir\n";
+}
+
+sub add_command {
+  my $index_loc = ".legit/index";
+  # Check if have init'd yet
+  if( ! -f $index_loc) {
+    print STDERR "$script_name: error: no .legit directory containing legit repository exists\n";
+    exit 1;
+  }
+  
+  # Check at least one file to add
+  if($#ARGV < 0) {
+    print STDERR "$script_name: error: internal error Nothing specified, nothing added.
+Maybe you wanted to say 'git add .'?
+
+You are not required to detect this error or produce this error message.\n";
+    exit 1;
+  }
+  
+  # Check if all files valid
+  foreach my $file (@ARGV) {
+    # Check filename has no invalid chars
+    if(!($file =~ /^[a-z0-9]/i && $file =~ /^[a-z0-9\-_\.]+$/i)) {
+      print "invalid file\n";
+    }
+  }
+  
+  # Load current index into hash
+  
+  # Add new files to hash (this will remove dupes)
+  # Write the hash to index
+}
+
